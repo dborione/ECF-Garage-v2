815ee0265f078fdc7a84591c03498a6b45d4dd66
dborione <db1997@outlook.fr>
2024-02-12 10:50:56 +0100

    add unit tests

--- tests/EmployeeUnitTest.php
+++ tests/EmployeeUnitTest.php
@@ -2,12 +2,38 @@
 
 namespace App\Tests;
 
+use App\Entity\Employee;
 use PHPUnit\Framework\TestCase;
 
 class EmployeeUnitTest extends TestCase
 {
-    public function testSomething(): void
+    public function testIsTrue()
     {
-        $this->assertTrue(true);
+        $employee = new Employee();
+
+        $employee->setEmail('true@test.com');
+        $employee->setPassword('password');
+
+        $this->assertTrue($employee->getEmail() === 'true@test.com');
+        $this->assertTrue($employee->getPassword() === 'password');
+    }
+
+    public function testIsFalse()
+    {
+        $employee = new Employee();
+
+        $employee->setEmail('true@test.com');
+        $employee->setPassword('password');
+
+        $this->assertFalse($employee->getEmail() === 'false@test.com');
+        $this->assertFalse($employee->getPassword() === 'false');
+    }
+
+    public function testIsEmpty()
+    {
+        $employee = new Employee();
+
+        $this->assertEmpty($employee->getEmail());
+        // $this->assertEmpty($employee->getPassword());
     }
 }
