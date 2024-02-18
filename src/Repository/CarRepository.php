00b6f416748db518651e3df03d9857e2a2a7ab28
dborione <db1997@outlook.fr>
2024-02-18 15:19:33 +0100

    front end for home cars section ok

--- src/Repository/CarRepository.php
+++ src/Repository/CarRepository.php
@@ -21,30 +21,30 @@ class CarRepository extends ServiceEntityRepository
         parent::__construct($registry, Car::class);
     }
 
-   /**
-    * @return Car[] Returns an array of Car objects
-    */
-   public function findByExampleField($value): array
-   {
-       return $this->createQueryBuilder('c')
-           ->andWhere('c.exampleField = :val')
-           ->setParameter('val', $value)
-           ->orderBy('c.id', 'ASC')
-           ->setMaxResults(10)
-           ->getQuery()
-           ->getResult()
-       ;
-   }
+//    /**
+//     * @return Car[] Returns an array of Car objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
 
-   public function findOneBySomeField($value): ?Car
-   {
-       return $this->createQueryBuilder('c')
-           ->andWhere('c.exampleField = :val')
-           ->setParameter('val', $value)
-           ->getQuery()
-           ->getOneOrNullResult()
-       ;
-   }
+//    public function findOneBySomeField($value): ?Car
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
 
     /**
     * @return Car[] Returns last ten Car objects
@@ -53,7 +53,7 @@ class CarRepository extends ServiceEntityRepository
     {
         return $this->createQueryBuilder('c')
             ->orderBy('c.id', 'DESC')
-            ->setMaxResults(10)
+            ->setMaxResults(8)
             ->getQuery()
             ->getResult()
         ;
