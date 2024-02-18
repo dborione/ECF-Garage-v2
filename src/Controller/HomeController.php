beb732734796928d15f8d41ce10ebd7916ba0ffb
dborione <db1997@outlook.fr>
2024-02-18 11:24:17 +0100

    add missing use in home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ReviewRepository;
+use App\Repository\CarRepository;
 
 class HomeController extends AbstractController
 {
