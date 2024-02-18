a852a6cb012276bd23a98ff5d2647b72934e35c8
dborione <db1997@outlook.fr>
2024-02-18 12:12:02 +0100

    only show approved reviews on homepage

--- src/Repository/ReviewRepository.php
+++ src/Repository/ReviewRepository.php
@@ -21,37 +21,41 @@ class ReviewRepository extends ServiceEntityRepository
         parent::__construct($registry, Review::class);
     }
 
-   /**
-    * @return Review[] Returns an array of Review objects
-    */
-   public function findByExampleField($value): array
-   {
-       return $this->createQueryBuilder('r')
-           ->andWhere('r.exampleField = :val')
-           ->setParameter('val', $value)
-           ->orderBy('r.id', 'ASC')
-           ->setMaxResults(10)
-           ->getQuery()
-           ->getResult()
-       ;
-   }
+//    /**
+//     * @return Review[] Returns an array of Review objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('r.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
 
-   public function findOneBySomeField($value): ?Review
-   {
-       return $this->createQueryBuilder('r')
-           ->andWhere('r.exampleField = :val')
-           ->setParameter('val', $value)
-           ->getQuery()
-           ->getOneOrNullResult()
-       ;
-   }
+//    public function findOneBySomeField($value): ?Review
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
 
       /**
     * @return Review[] Returns last five Review objects
     */
     public function findLastFive()
     {
+        $status = 'approved';
+
         return $this->createQueryBuilder('r')
+            ->where('r.status = :status')
+            ->setParameter('status', $status)
             ->orderBy('r.id', 'DESC')
             ->setMaxResults(5)
             ->getQuery()
