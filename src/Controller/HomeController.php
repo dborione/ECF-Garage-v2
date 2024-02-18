b7c78d55ac9bc9611ebe02baddd7de6cec27fea3
dborione <db1997@outlook.fr>
2024-02-18 11:17:04 +0100

    add cars and reviews in home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,10 +9,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(ReviewRepository $reviewRepository, CarRepository $carRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'reviews' => $reviewRepository->findLastFive(),
+            'cars' => $carRepository->findLastTen(),
         ]);
     }
 }
