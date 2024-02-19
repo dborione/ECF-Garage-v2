d1cdc245623cee2c1bc1ba8a661e6171b3d04196
dborione <db1997@outlook.fr>
2024-02-19 18:19:42 +0100

    add front for single_car not finished

--- src/Controller/CarsController.php
+++ src/Controller/CarsController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\CarRepository;
 
-
 class CarsController extends AbstractController
 {
     #[Route('/cars', name: 'app_cars')]
@@ -19,11 +18,12 @@ class CarsController extends AbstractController
     }
 
     #[Route('/cars/{slug}', name: 'app_cars_slug', methods: ['GET'])]
-    public function seeCar(string $slug): Response
+    public function seeCar(CarRepository $carRepository, string $slug): Response
     {
-        // return $this->render('cars/index.html.twig', [
-        //     'cars' => $carRepository->findAll(),
-        // ]);
-        dd($slug);
+        // dd($slug);
+        return $this->render('cars/single_car.html.twig', [
+            'car' => $carRepository->findOneBySomeField($slug),
+            // 'car' => $carRepository->findAll(),
+        ]);
     }
 }
