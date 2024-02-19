2f170aec168e6ced8876459c580ec15e99297240
dborione <db1997@outlook.fr>
2024-02-19 18:52:30 +0100

    install htmx and preline js

--- assets/app.js
+++ assets/app.js
@@ -5,5 +5,9 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import htmx from 'htmx.org';
+import 'preline';
+
+window.htmx = htmx;
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
