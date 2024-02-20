c42a063846a9c99e1bf2611562e9b305e42a40de
dborione <db1997@outlook.fr>
2024-02-20 15:44:18 +0100

    form working

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -4,15 +4,40 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Review;
+use App\Form\ReviewType;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ReviewController extends AbstractController
 {
     #[Route('/review', name: 'app_review')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $review = new Review();
+
+        $form = $this->createForm(ReviewType::class, $review);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $review = new Review();
+            $review->setName($form->get('name')->getData());
+            $review->setNote($form->get('note')->getData());
+            $review->setBody($form->get('body')->getData());
+            $review->setStatus('pending');
+
+            // $review = $form->getData();
+            
+            $entityManager->persist($review);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_review');
+        }
+
         return $this->render('review/index.html.twig', [
-            'controller_name' => 'ReviewController',
+            'reviewForm' => $form,
         ]);
     }
 }
