9c4955dfa6a5eb2844d6662c6c688e96abd2bbe4
dborione <db1997@outlook.fr>
2024-02-20 19:51:58 +0100

    all forms ok

--- src/Controller/CarsController.php
+++ src/Controller/CarsController.php
@@ -4,8 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\CarRepository;
+use App\Entity\ContactMessage;
+use App\Form\ContactMessageType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CarsController extends AbstractController
 {
@@ -14,16 +18,33 @@ class CarsController extends AbstractController
     {
         return $this->render('cars/index.html.twig', [
             'cars' => $carRepository->findAll(),
+            'highest_km' => $carRepository->highestKm(),
+            'lowest_km' => $carRepository->lowestKm(),
         ]);
     }
 
-    #[Route('/cars/{slug}', name: 'app_cars_slug', methods: ['GET'])]
-    public function seeCar(CarRepository $carRepository, string $slug): Response
+    #[Route('/cars/{slug}', name: 'app_cars_slug')]
+    public function seeCar(Request $request, EntityManagerInterface $entityManager, CarRepository $carRepository, string $slug): Response
     {
-        // dd($slug);
+        $contactMessage = new ContactMessage();
+
+        $form = $this->createForm(ContactMessageType::class, $contactMessage);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactMessage = new ContactMessage();
+
+            $contactMessage = $form->getData();
+            
+            $entityManager->persist($contactMessage);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_cars_slug', ['slug'=>$slug]);
+        }
+
         return $this->render('cars/single_car.html.twig', [
-            'car' => $carRepository->findOneBySomeField($slug),
-            // 'car' => $carRepository->findAll(),
+            'cars' => $carRepository->findBy(['slug'=>$slug]),
+            'contactForm' => $form,
         ]);
     }
 }
