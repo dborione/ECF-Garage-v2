d2ba187be27c43975c5ec27f78f76239b8b273f6
dborione <db1997@outlook.fr>
2024-02-21 08:58:37 +0100

    add balloon

--- assets/app.js
+++ assets/app.js
@@ -12,3 +12,42 @@ import 'preline';
 window.htmx = htmx;
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+let height = 20;
+let width = 20;
+let colors = ["red", "green", "blue"];
+let i = 0;
+function chSquare() {
+    height += 10;
+    width += 10;
+    if (height >= 420 || width >= 420) {
+        height = 20;
+        width = 20;
+    }
+    let hStr = width.toString();
+    let wStr = width.toString();
+    document.getElementById("square").style.width = wStr + "px";
+    document.getElementById("square").style.height = hStr + "px";
+    i += 1;
+    if (i == 3)
+        i = 0;
+    document.getElementById("square").style.backgroundColor = colors[i];
+}
+function outMouse() {
+    height -= 5;
+    width -= 5;
+    if (height <= 200 || width <= 200) {
+        height = 20;
+        width = 20;
+    }
+    let hStr = width.toString();
+    let wStr = width.toString();
+    document.getElementById("square").style.width = wStr + "px";
+    document.getElementById("square").style.height = hStr + "px";
+    if (i == 0)
+        i = 3;
+    i -= 1;
+    document.getElementById("square").style.backgroundColor = colors[i];
+}
+document.getElementById("square").addEventListener("click", chSquare);
+document.getElementById("square").addEventListener("mouseout", outMouse);
